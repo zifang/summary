68abc5d78542d569fea4cf6cb0b874a217585a23
fangdongxia <fangdongxia@niwodai.net>
2017-12-19 14:51:28 +0800

    'edit'

--- src/pages/jsPage/summeryJs/fileupload.js
+++ src/pages/jsPage/summeryJs/fileupload.js
@@ -27,3 +27,57 @@ ps.uploadLogo = function() {
   });
   return false;
 }
+
+
+// ajaxfileupload
+<div class="control-group">
+  <label class="control-label"><span class="colorg">＊</span>品牌LOGO：</label>
+  <div class="controls">
+      <!-- <input class="input-xlarge postfile" type="file"> -->
+      <a href="javascript:;" class="a-upload" id="uploadWrapper">
+        <input type="file" name="storePic" id="storePic" onchange="ajaxFileUploadChange()">
+      </a>
+      <span class="mr10 text-gray">尺寸：大于600*600px、大小：100kb以内</span>
+  </div>
+</div>
+
+function ajaxFileUploadChange(){
+
+    //$("#loading").ajaxStart(function(){$(this).show();}).ajaxComplete(function(){$(this).hide();});
+   //执行上传文件操作的函数
+    $.ajaxFileUpload({
+     url:'/anne/storelist/uploadAction',
+     secureuri:false,                           //是否启用安全提交,默认为false 
+        fileElementId:'storePic',               //文件选择框的id属性
+        dataType:'text',                           //服务器返回的格式,可以是json或xml等
+        success:function(data, status){            //服务器响应成功时的处理函数
+            data = data.replace(/<pre.*?>/g, '');  //ajaxFileUpload会对服务器响应回来的text内容加上<pre style="....">text</pre>前后缀
+            data = data.replace(/<PRE.*?>/g, '');
+            data = data.replace("<PRE>", '');
+            data = data.replace("</PRE>", '');
+            data = data.replace("<pre>", '');
+            data = data.replace("</pre>", '');     //本例中设定上传文件完毕后,服务端会返回给前台[0`filepath]
+            if(data!="" ){         //0表示上传成功(后跟上传后的文件路径),1表示失败(后跟失败描述)
+               //$("img[id='uploadImage']").attr("src", data.substring(2));
+              
+               $("#brandLogoUrl").val(data);
+               $("#uploadWrapper").css({
+                "background":"url('"+ data +"')"
+               });
+               if(data.length>120){
+                //alert("图片上传文件过大!");
+                alertBox.show("error","图片上传文件过大!",false,"","",true);
+               }else{
+                // alert("图片上传成功!");
+                //alertBox.show("success","图片上传成功！",false,"","",true);
+               }
+            }else{
+              //alert("图片上传失败，请重试！");
+              alertBox.show("error","图片上传失败，请重试！",false,"","",true);
+            }
+        },
+        error:function(data, status, e){ //服务器响应失败时的处理函数
+            $('#result').html('图片上传失败，请重试！！');
+        }
+   });
+}
